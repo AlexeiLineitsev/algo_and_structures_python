962b7d573c43a552812c3bcf24f709730c0829f2
alexei <alexei75chita@mail.ru>
2019-03-12 11:41:42 +0900

    3 и 4 задание hw1

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,26 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+
+import random
+
+print('Случайное целое число в диапазоне от А до B')
+a = int(input('A = '))
+b = int(input('B = '))
+
+print("Ваше число",random.randint(a,b))
+
+
+print('Вещественное целое число в диапазоне от А до B')
+a = float(input('A = '))
+b = float(input('B = '))
+
+print("Ваше число %.2f" % random.uniform(a,b))
+
+
+print('Cлучайный символ число в диапазоне от А до B')
+a = ord(input('A = '))
+b = ord(input('B = '))
+
+print("Cлучайный символ ", chr(random.randint(a,b)))
+
